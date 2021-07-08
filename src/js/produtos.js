07dfc20ed6b24c79ef01e955e4a88f4102aeb946
Luiz C M <81635361+LuizCMbh@users.noreply.github.com>
2021-07-08 11:24:49 +0000

    Funcionalidades, ajustes finais do localstorage, filtros

--- src/js/produtos.js
+++ src/js/produtos.js
@@ -1,3 +1,4 @@
+// ARRAY JSON PRODUTOS
 var produtos = {
 	"categorias": [
     {
@@ -31,9 +32,8 @@ var produtos = {
   ]
 }
 
-const inputPesquisa = document.querySelector("#inputPesquisa");
-const inputSubmit = document.querySelector("#btnPesquisa");
-
+// DECLARACAO DO ARRAY JSON CRIADO EM PARTES E ARMAZENAMENTO DELE NO LOCALSTORAGE
+let inputSubmit = document.querySelector("#btnPesquisa");
 let produtosloja = new Array()
 let produtosvestuarios = new Array()
 let produtosmarcas = new Array()
@@ -47,8 +47,6 @@ produtoscor = produtos.categorias[0].cor
 produtostamanho = produtos.categorias[0].tamanho
 
 inputSubmit.addEventListener("click", _ => {
-
-	
 
 	if (localStorage.hasOwnProperty("produtosloja")) {
     	produtosloja = JSON.parse(localStorage.getItem("produtosloja"))
@@ -70,9 +68,6 @@ inputSubmit.addEventListener("click", _ => {
     	produtostamanho = JSON.parse(localStorage.getItem("produtostamanho"))
   	}
 
-  	/* Adiciona um novo valor nos arrays criados */
-
-		//produtos.push({cep: inputCEP.value, endereco: inputEndereco.value, bairro: inputBairro.value, cidade: inputCidade.value, estado: inputEstado.value})
     localStorage.setItem("produtosloja", JSON.stringify(produtosloja))
 		localStorage.setItem("produtosvestuarios", JSON.stringify(produtosvestuarios))
 		localStorage.setItem("produtosmarcas", JSON.stringify(produtosmarcas))
@@ -80,211 +75,121 @@ inputSubmit.addEventListener("click", _ => {
 		localStorage.setItem("produtostamanho", JSON.stringify(produtostamanho))
 })
 
+/* -- */
 
-/* PRIMEIRA PARTE
+// DECLARACAO DO INPUT E BOTAO DE PESQUISA
+var pesquisa = document.getElementById("inputPesquisa");
+var botaoPesquisa = document.getElementById("btnPesquisa");
 
-function adicionaItem(e) {
+// DECLARACAO DOS CARDS DE CADA PRODUTO
+var primeiraCol = document.getElementById("primeiraCol");
+var segundaCol = document.getElementById("segundaCol");
+var terceiraCol = document.getElementById("terceiraCol");
+var quartaCol = document.getElementById("quartaCol");
+var quintaCol = document.getElementById("quintaCol");
+var sextaCol = document.getElementById("sextaCol");
+var setimaCol = document.getElementById("setimaCol");
+var oitavaCol = document.getElementById("oitavaCol");
+var semEstoque = document.getElementById("semEstoque");
 
-  document.querySelector("#inputPesquisa").innerHTML = pesquisa_select;
-  var pesquisa_select = produtosvestuarios || produtosmarcas || produtoscor || produtostamanho;
-	
-	 //document.querySelector("#inputPesquisa");
+// FUNCAO QUE ALTERA O PRODUTO QUE APARECE NA VITRINE DE ACORDO COM O PRODUTO PESQUISADO
+document.getElementById("inputPesquisa").addEventListener("change", pesquisaProduto);
 
-  let num_produtos = pesquisa_select;
-  var j_index = -1;
+	function pesquisaProduto() {
 
-   // INDEX DO PRODUTO DENTRO DO JSON ARRAY
-  for(var x = 0; x < num_produtos; x++) {
-  	if(produtos.categorias[0].vestuarios[x].valueOf || produtos.categorias[0].marcas[x].valueOf || produtos.categorias[0].cor[x].valueOf || produtos.categorias[0].tamanho[x].valueOf == e) {
-         j_index = x;
-    }
+		if ((pesquisa.value == "vestidos") || (pesquisa.value == "vestido")) {
+
+			document.getElementById("btnPesquisa").addEventListener("click", pesquisaProdutoBotao);
+
+			function pesquisaProdutoBotao() {
+				event.preventDefault()
+				primeiraCol.classList.replace("displayn", "displayb");
+				segundaCol.classList.replace("displayn", "displayb");
+				terceiraCol.classList.replace("displayn", "displayb");
+				quartaCol.classList.replace("displayn", "displayb");
+				quintaCol.classList.replace("displayb", "displayn");
+				sextaCol.classList.replace("displayb", "displayn");
+				setimaCol.classList.replace("displayb", "displayn");
+				oitavaCol.classList.replace("displayn", "displayb");
+				semEstoque.classList.replace("displayb", "displayn");
+			};
+		}
+
+		if ((pesquisa.value == "saia") || (pesquisa.value == "saias")) {
+
+			document.getElementById("btnPesquisa").addEventListener("click", pesquisaProdutoBotao);
+
+			function pesquisaProdutoBotao() {
+				event.preventDefault()
+				primeiraCol.classList.replace("displayb", "displayn");
+				segundaCol.classList.replace("displayb", "displayn");
+				terceiraCol.classList.replace("displayb", "displayn");
+				quartaCol.classList.replace("displayb", "displayn");
+				quintaCol.classList.replace("displayn", "displayb");
+				sextaCol.classList.replace("displayb", "displayn");
+				setimaCol.classList.replace("displayn", "displayb");
+				oitavaCol.classList.replace("displayb", "displayn");
+				semEstoque.classList.replace("displayb", "displayn");
+			};
+		}
+
+		if ((pesquisa.value == "blusa") || (pesquisa.value == "blusas") || (pesquisa.value == "casaco")) {
+
+			document.getElementById("btnPesquisa").addEventListener("click", pesquisaProdutoBotao);
+
+			function pesquisaProdutoBotao() {
+				event.preventDefault()
+				primeiraCol.classList.replace("displayb", "displayn");
+				segundaCol.classList.replace("displayb", "displayn");
+				terceiraCol.classList.replace("displayb", "displayn");
+				quartaCol.classList.replace("displayb", "displayn");
+				quintaCol.classList.replace("displayb", "displayn");
+				sextaCol.classList.replace("displayb", "displayn");
+				setimaCol.classList.replace("displayn", "displayb");
+				oitavaCol.classList.replace("displayb", "displayn");
+				semEstoque.classList.replace("displayb", "displayn");
+			};
+		}
+
+		if ((pesquisa.value == "top") || (pesquisa.value == "short") || (pesquisa.value == "shorts")) {
+
+			document.getElementById("btnPesquisa").addEventListener("click", pesquisaProdutoBotao);
+
+			function pesquisaProdutoBotao() {
+				event.preventDefault()
+				primeiraCol.classList.replace("displayb", "displayn");
+				segundaCol.classList.replace("displayb", "displayn");
+				terceiraCol.classList.replace("displayb", "displayn");
+				quartaCol.classList.replace("displayb", "displayn");
+				quintaCol.classList.replace("displayb", "displayn");
+				sextaCol.classList.replace("displayn", "displayb");
+				setimaCol.classList.replace("displayb", "displayn");
+				oitavaCol.classList.replace("displayb", "displayn");
+				semEstoque.classList.replace("displayb", "displayn");
+			};
+		}
+
+		if (pesquisa.value == "") {			
+				primeiraCol.classList.replace("displayn", "displayb");
+				segundaCol.classList.replace("displayn", "displayb");
+				terceiraCol.classList.replace("displayn", "displayb");
+				quartaCol.classList.replace("displayn", "displayb");
+				quintaCol.classList.replace("displayn", "displayb");
+				sextaCol.classList.replace("displayn", "displayb");
+				setimaCol.classList.replace("displayn", "displayb");
+				oitavaCol.classList.replace("displayn", "displayb");
+				semEstoque.classList.replace("displayb", "displayn");
+		}
+
+		else {
+			primeiraCol.classList.replace("displayb", "displayn");
+			segundaCol.classList.replace("displayb", "displayn");
+			terceiraCol.classList.replace("displayb", "displayn");
+			quartaCol.classList.replace("displayb", "displayn");
+			quintaCol.classList.replace("displayb", "displayn");
+			sextaCol.classList.replace("displayb", "displayn");
+			setimaCol.classList.replace("displayb", "displayn");
+			oitavaCol.classList.replace("displayb", "displayn");
+			semEstoque.classList.replace("displayn", "displayb");
+		}
 	}
-
-	if(j_index != -1) {
-  
-      // PASSA POR TODAS AS CIDADES DO ESTADO SELECIONADO,
-			// CRIA UM ELEMENTO OPTION E ADICIONA UM VALUE A TODAS ELAS
-
-    produtos.categorias[0].vestuarios[j_index].valueOf(function(vestuarios) {
-
-			if (localStorage.hasOwnProperty("produtosvestuarios")) {
-    			vestuarios = JSON.parse(localStorage.getItem("produtosvestuarios"));
-					e = vestuarios;
-  		}
-   	})
-  }
-
-	window.setTimeout(function() {
-
-					
-
-					document.getElementById("btnPesquisa").addEventListener("click", adicionaItem);
-
-					$("#listaprodutos").on("click", "li", function(event) {
-
-						var listaprodutos = $("#listaprodutos");
-						var itemlista = $(".itemlista");
-
-        		var index = listaprodutos.index(itemlista);
-        		var nth = "#listaprodutos li:nth-child("+(index+1).toString()+")";
-
-						var vestuarios = produtosvestuarios;
-
-						for(var x = 0; x < vestuarios; x++) {
-							let listaprodutos = produtos.categorias[0].vestuarios[x].valueOf();
-						}
-
-						if (localStorage.hasOwnProperty("produtosvestuarios")) {
-    					vestuarios = JSON.parse(localStorage.getItem("produtosvestuarios"));
-							e = vestuarios;
-  					}
-
-
-
-    				var elem = $(this);
-						event.preventDefault();
-    				console.log( elem.text() );
-					});
-	});
-}
-
-
-				/*
-    		$("#btnPesquisa").click(function() {
-
-					var listaprodutos = $("#listaprodutos");
-					var itemlista = $(".itemlista");
-
-        	var index = listaprodutos.index(itemlista);
-        	var nth = "#listaprodutos li:nth-child("+(index+1).toString()+")";
-        	//var valor = $(this).val().toUpperCase();
-
-				​​​​​​​​​​​​​​​​​​​​​*/
-					/*
-        	$("#listaprodutos li").show();
-        	$(nth).each(function() {
-            if($(this).text().toUpperCase().indexOf(valor) < 0) {
-                $(this).parent().hide();
-            }
-        	});
-					*/
-    		
- 
-    	//	$("#btnPesquisa").blur(function(){
-       // 	$(this).val("");
-    	//	});
-			//})
-			
-	
-
-
-/*
-window.setTimeout(function() {
-
-    		$("#btnPesquisa").click(function() {
-
-        	var index = $(this).parent().index();
-        	var nth = "#tabela td:nth-child("+(index+1).toString()+")";
-        	var valor = $(this).val().toUpperCase();
-        	$("#tabela tbody tr").show();
-        	$(nth).each(function() {
-            if($(this).text().toUpperCase().indexOf(valor) < 0) {
-                $(this).parent().hide();
-            }
-        	});
-    		});
- 
-    		$("#btnPesquisa").blur(function(){
-        	$(this).val("");
-    		});
-			})
-
-
-	*/		
-	/*
-	 $(function(){
-    $("#btnPesquisa").keyup(function(){        
-        var index = $(this).parent().index();
-        var nth = "#tabela td:nth-child("+(index+1).toString()+")";
-        var valor = $(this).val().toUpperCase();
-        $("#tabela tbody tr").show();
-        $(nth).each(function(){
-            if($(this).text().toUpperCase().indexOf(valor) < 0){
-                $(this).parent().hide();
-            }
-        });
-    });
- 
-    $("#btnPesquisa").blur(function(){
-        $(this).val("");
-    }); 
-		*/
-
-/*
-         var cid_opts = document.createElement('option');
-         cid_opts.setAttribute('value',cidade)
-         cid_opts.innerHTML = cidade;
-         cidade_select.appendChild(cid_opts);
-      	});
-
-				
-
-
-
-
-*/
-
-
-/* SEGUNDA PARTE
-
-    function AdicionaItem() {
-            
-						
-
-            // Obtem os dados informados pelo usuário nos filtros
-            //let fc = document.getElementById('filtro_cidade').value;
-            //let fcg = document.getElementById('filtro_categoria').value;
-
-						let pesquisa = document.getElementById("inputPesquisa").value;
-
-            // limpa a lista de contatos apresentados
-            //$("#table-contatos").empty();
-
-            // Popula a tabela com os registros do banco de dados
-            //for (let index = 0; index < db.data.length; index++) {
-                //const contato = db.data[index];
-
-						for (let index = 0; index < produtosvestuarios.length; index++) {
-                const vestuarios = produtosvestuarios[index];
-
-								
-
-                // Verifica se os dados do contato batem com os filtros
-                if ((vestuarios.blusas == pesquisa) || (vestuarios.vestidos == pesquisa))   {
-                    
-                    // Inclui o contato na tabela    
-                    $("#listaprodutos").append(`<li class="itemlista">${vestuarios.blusas}</li>
-                                                    <li>${vestuarios.vestidos}</li>`);
-                }
-
-/*
-								if (((contato.cidade == fc) || (fc == '')) &&
-                   ((contato.categoria == fcg) || (fcg == '')))   {
-                    
-                    // Inclui o contato na tabela    
-                    $("#table-contatos").append(`<tr><td scope="row">${contato.id}</td>
-                                                    <td>${contato.nome}</td>
-                                                    <td>${contato.telefone}</td>
-                                                    <td>${contato.email}</td>
-                                                    <td>${contato.cidade}</td>
-                                                    <td>${contato.categoria}</td>
-                                                    <td>${contato.website}</td>
-                                                </tr>`);
-                }
-
-
-            }
-
-        }
-
-				*/
